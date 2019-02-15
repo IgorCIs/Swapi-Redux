d2a2039b67013d177424c0931eba974e415941ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-15 22:00:24 +0000

    Deploy IgorCIs/Swapi-Redux to github.com/IgorCIs/Swapi-Redux.git:gh-pages

---
+++ precache-manifest.119150c5c82ca0e84d8013b184d8263a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ed3cf1269747f363b3",
+    "url": "/Swapi-Redux/static/js/runtime~main.42ed3cf1.js"
+  },
+  {
+    "revision": "b6003ba10f791ab15108",
+    "url": "/Swapi-Redux/static/js/main.b6003ba1.chunk.js"
+  },
+  {
+    "revision": "c6c500c8379fbff17a2d",
+    "url": "/Swapi-Redux/static/js/2.c6c500c8.chunk.js"
+  },
+  {
+    "revision": "b6003ba10f791ab15108",
+    "url": "/Swapi-Redux/static/css/main.c1a91c3a.chunk.css"
+  },
+  {
+    "revision": "82925b117f3f7da67056ed7160369c01",
+    "url": "/Swapi-Redux/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33d5480bff27ddbf8d0f13308de88839.js"
+  "/Swapi-Redux/precache-manifest.119150c5c82ca0e84d8013b184d8263a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Swapi-Redux/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
