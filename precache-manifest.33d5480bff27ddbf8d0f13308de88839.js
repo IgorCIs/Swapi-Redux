d249d448d99d31b99142396dcd4fd58fd74f1e72
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-15 21:53:06 +0000

    Deploy IgorCIs/Swapi-Redux to github.com/IgorCIs/Swapi-Redux.git:gh-pages

---
+++ precache-manifest.33d5480bff27ddbf8d0f13308de88839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b6003ba10f791ab15108",
+    "url": "/static/js/main.b6003ba1.chunk.js"
+  },
+  {
+    "revision": "c6c500c8379fbff17a2d",
+    "url": "/static/js/2.c6c500c8.chunk.js"
+  },
+  {
+    "revision": "b6003ba10f791ab15108",
+    "url": "/static/css/main.c1a91c3a.chunk.css"
+  },
+  {
+    "revision": "f71e8803d2d33a7d13491d7b05dc7794",
+    "url": "/index.html"
+  }
+];
